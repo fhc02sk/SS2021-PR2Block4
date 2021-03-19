f3a6264a9cb9c7a0324a2f2ae1b1f82e65303f15
Stephan Kochauf <stephan.kochauf@lv.campus02.at>
2021-03-19 19:11:48 +0100

    String-Output

--- src/org/campus02/objectoutput/DemoStringOutputApp.java
+++ src/org/campus02/objectoutput/DemoStringOutputApp.java
@@ -1,6 +1,28 @@
 package org.campus02.objectoutput;
 
+import java.io.*;
+
+
 public class DemoStringOutputApp {
 
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+        String path = "D:\\temp\\string-output.dat";
+        /*FileOutputStream fos = new FileOutputStream("D:\\temp\\string-output.dat");
+
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+        String demoText = "Hello File World!";
+        oos.writeObject(demoText);
+        oos.flush();
+        oos.close();
+        */
+
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(path));
+        String demoText = (String)ois.readObject();
+        ois.close();
+        System.out.println("demoText = " + demoText);
+
+    }
 
 }
